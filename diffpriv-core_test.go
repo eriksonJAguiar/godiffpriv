3c4d1bf221e04a368213e7e86387c21a2750adfb
EriksonJAguiar <erjulioaguiar@gmail.com>
2020-07-08 22:37:33 -0300

    draft v1.0

---
+++ diffpriv-core_test.go
@@ -0,0 +1,41 @@
+package godiffpriv
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestDiffPrivSymbolic(t *testing.T) {
+	data := []string{"Male", "Female", "Male", "Female"}
+	val := PrivateDataFactory(data)
+	res, _ := val.applyPrivacy(1)
+
+	var response map[string]float64
+
+	err := json.Unmarshal(res, &response)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(response)
+	}
+
+}
+
+func TestDiffPrivNumeric(t *testing.T) {
+	data := []float64{1.5, 2.3, 7.2, 9.1}
+	val := PrivateDataFactory(data)
+	res, _ := val.applyPrivacy(1)
+
+	var response map[string]float64
+
+	err := json.Unmarshal(res, &response)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(response)
+	}
+
+}
